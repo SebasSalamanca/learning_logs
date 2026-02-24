6170524a6aaff6f26c5734908c89ecbf0d0d3742
sebas_salamanca24 <jsebastian1119@gmail.com>
2026-02-23 19:01:49 -0500

    the topics template

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
 #Render the response based on the data provided by views 
 from django.shortcuts import render
 
+from .models import Topic
+
 # Create your views here.
 """A view function takes in information from a request, prepares the data needed to generate a page, 
 and then sends the data back to the browser. """
@@ -10,3 +12,8 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.order_by("date_added")
+    context = {"topics": topics}
+    return render(request, 'learning_logs/topics.html', context)
